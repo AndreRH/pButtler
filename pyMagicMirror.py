8b667a1d7fb58b7c5fcba0af7f18afd384c5b921
André Hentschel <nerv@dawncrow.de>
2017-04-26 20:47:25 +0200

    Remove fading and screensaver features

--- pyMagicMirror.py
+++ pyMagicMirror.py
@@ -24,29 +24,8 @@ pygame.init()
 # Set the screen to fullscreen
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 
-pygame.display.set_caption("buttler")
-
 # Init the main surface
 surface = pygame.Surface(screen.get_size())
-surface.set_alpha(255)
-
-# Init the fading properties
-(FADE_IN, FADE_OUT, FADE_STOP) = (1, -1, 0)
-fadeDir = FADE_STOP
-fadeSpeed = 5
-
-# Init the screensaver properties
-saverTimer = 60000   # Time without activity to trigger the screensaver
-
-# Define custom events
-FADING = pygame.USEREVENT +1
-event_fadeIn = pygame.event.Event(FADING, dir = FADE_IN)
-event_fadeOut = pygame.event.Event(FADING, dir = FADE_OUT)
-event_fadeStop = pygame.event.Event(FADING, dir = FADE_STOP)
-
-SAVER = pygame.USEREVENT +2
-event_saver = pygame.event.Event(SAVER)
-pygame.time.set_timer(SAVER, saverTimer)
 
 # Init widgets
 widgets = {
@@ -77,51 +56,20 @@ while not mustQuit:
             mustQuit = True
 
         if event.type == pygame.KEYUP:
-            # Update the screensaver activity tracker
-            pygame.time.set_timer(SAVER, saverTimer)
-
             if event.key == pygame.K_ESCAPE:
                 mustQuit = True
-            if event.key == pygame.K_UP:
-                pygame.event.post(event_fadeIn)
-            if event.key == pygame.K_DOWN:
-                pygame.event.post(event_fadeOut)
 
-        # Solve fading events
-        if event.type == FADING:
-            fadeDir = event.dir
+    # Clear the screen to black
+    screen.fill(c_BLACK)
+    surface.fill(c_BLACK)
 
-        # Solve saver events
-        if event.type == SAVER:
-            pygame.event.post(event_fadeOut)
+    # Update widgets
+    for widget in widgets.values():
+        widget.update(surface, t)
 
-    # Update the fading if necessary
-    if fadeDir != FADE_STOP:
-        a = surface.get_alpha() + fadeDir*tt/fadeSpeed
-        if a < 0:
-            a = 0
-            fadeDir = FADE_STOP
-        elif a > 255:
-            a = 255
-            fadeDir = FADE_STOP
-        surface.set_alpha(a)
-
-    # Screensaver
-    if False and surface.get_alpha() == 0:
-        os.system("xset dpms force off")
-    else:
-        # --- Drawing code
-        # Clear the screen to black
-        screen.fill(c_BLACK)
-        surface.fill(c_BLACK)
-
-        # Update widgets
-        for widget in widgets.values():
-            widget.update(surface, t)
-
-        # Update screen
-        screen.blit(surface, (0, 0))
-        pygame.display.flip()
+    # Update screen
+    screen.blit(surface, (0, 0))
+    pygame.display.flip()
 
 # Close the windows and quit
 pygame.quit()
